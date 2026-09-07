5931b8e57110f95cf1e5d9eaf57f4aba818ba12d
Khoa <minhkhoajapan0701@gmail.com>
2026-09-07 19:57:17 +0900

    KeycloakAPIを呼ぶ専用クラス

---
+++ src/main/java/com/example/KeycloakPOC/utility/KeycloakClient.java
@@ -0,0 +1,48 @@
+package com.example.KeycloakPOC.utility;
+
+import com.example.KeycloakPOC.entity.AppUser;
+import org.springframework.stereotype.Component;
+import tools.jackson.databind.JsonNode;
+import tools.jackson.databind.ObjectMapper;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+@Component
+public class KeycloakClient {
+
+    private static final String KEYCLOAK_PATH = "http://localhost:9090";
+    private static final String REALM_TEST = "test";
+
+    private static final ObjectMapper mapper = new ObjectMapper();
+    private static final HttpClient http = HttpClient.newHttpClient();
+
+    public AppUser getUserInfo(String accessToken) throws Exception {
+        JsonNode appUserJsonNode = getUserInfoJsonNode(accessToken);
+        System.out.println(appUserJsonNode.toPrettyString());
+
+        AppUser appUser = new AppUser();
+        appUser.setIdentityToken(appUserJsonNode.get("sub").asString());
+        appUser.setName(appUserJsonNode.get("name").asString());
+        appUser.setGivenName(appUserJsonNode.get("given_name").asString());
+        appUser.setFamilyName(appUserJsonNode.get("family_name").asString());
+        appUser.setEmail(appUserJsonNode.get("email").asString());
+        appUser.setPersonalMessage("fuck yeah it is working!");
+        return appUser;
+    }
+
+    private JsonNode getUserInfoJsonNode(String accessToken) throws Exception {
+        String userInfoURI = KEYCLOAK_PATH + "/realms/" + REALM_TEST + "/protocol/openid-connect/userinfo";
+
+        HttpRequest req =  HttpRequest.newBuilder()
+                .uri(URI.create(userInfoURI))
+                .header("Authorization", "Bearer " + accessToken)
+                .GET()
+                .build();
+
+        HttpResponse<String> res = http.send(req, HttpResponse.BodyHandlers.ofString());
+        return mapper.readTree(res.body());
+    }
+}
